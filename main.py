145e92d4ab9a071a62e6de38d86034b88fae64ff
a-zuev <antondotnet2@gmail.com>
2017-04-13 07:24:30 +0300

    +плохой левенштейн

---
+++ main.py
@@ -0,0 +1,36 @@
+from trie import Trie
+from levenstein import LevenhsteinAutomaton
+
+def extract(Automaton, state, node):
+    if not Automaton.can_match(state):
+        return []
+    ans = []
+    if Automaton.is_match(state) and node.is_end:
+        ans.append('')
+    for c in node.children:
+        word_list = extract(Automaton, Automaton.step(state, c), node.children[c])
+        b = [c + w for w in word_list]
+        ans = ans + b
+    return ans
+
+def extract_words(node, string, n):
+    Automaton = LevenshteinAutomaton(string, n)
+    state = Automaton.start()
+    return extract(Automaton, state, node)
+
+
+T = Trie()
+while 1:
+    word = input()
+    if word == '$$$':
+        print('finaly')
+        break
+    print(T.check_word(word))
+    T.add_word(word)
+while 1:
+    word = input()
+    if word == '$$$':
+        print('finaly')
+        exit()
+    print(extract_words(T, word, 2))
+
